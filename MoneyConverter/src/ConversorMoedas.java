05b5db99e1f252b804f1f852d73995931120a2cc
Jorge <103595844+jorgelucasp42@users.noreply.github.com>
2024-06-06 20:16:44 -0300

    update

--- MoneyConverter/src/ConversorMoedas.java
+++ MoneyConverter/src/ConversorMoedas.java
@@ -11,61 +11,75 @@ import java.util.concurrent.CompletableFuture;
 public class ConversorMoedas {
 
     private static final String CHAVE_API = "b86236aee1a93e371769aa06"; // Chave API
-    private static final String URL_API = "https://v6.exchangerate-api.com/v6/" + CHAVE_API + "/latest/USD";
+    private static final String URL_API = "https://v6.exchangerate-api.com/v6/" + CHAVE_API + "/latest/BRL";
     private static final Gson gson = new Gson();
+    private static final HttpClient cliente = HttpClient.newHttpClient();
+    private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        HttpClient cliente = HttpClient.newHttpClient();
-        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            exibirMenu();
+            int escolha = scanner.nextInt();
 
-        System.out.println("Bem-vindo ao Conversor de Moedas!");
-        System.out.println("1. USD para EUR");
-        System.out.println("2. USD para GBP");
-        System.out.println("3. USD para JPY");
-        System.out.println("4. USD para AUD");
-        System.out.println("5. USD para CAD");
-        System.out.println("6. USD para BRL");
-        System.out.print("Escolha uma opção: ");
-
-        int escolha = scanner.nextInt();
-        String moedaDestino = obterMoedaDestino(escolha);
-
-        if (moedaDestino != null) {
-            try {
-                HttpRequest requisicao = HttpRequest.newBuilder()
-                        .uri(URI.create(URL_API))
-                        .build();
-
-                CompletableFuture<HttpResponse<String>> respostaFutura = cliente.sendAsync(requisicao, HttpResponse.BodyHandlers.ofString());
-
-                respostaFutura.thenApply(HttpResponse::body)
-                        .thenAccept(respostaJson -> {
-                            double taxaDeCambio = parseTaxaDeCambio(respostaJson, moedaDestino);
-                            System.out.print("Insira o valor em USD: ");
-                            double valor = scanner.nextDouble();
-                            double valorConvertido = valor * taxaDeCambio;
-                            System.out.println("Valor convertido: " + valorConvertido + " " + moedaDestino);
-                        }).join();
-            } catch (Exception e) {
-                e.printStackTrace();
+            if (escolha == 7) {
+                System.out.println("Saindo...");
+                break;
             }
-        } else {
-            System.out.println("Opção inválida!");
+
+            String moedaDestino = obterMoedaDestino(escolha);
+            if (moedaDestino != null) {
+                realizarConversao(moedaDestino);
+            } else {
+                System.out.println("Opção inválida!");
+            }
         }
 
         scanner.close();
     }
 
+    private static void exibirMenu() {
+        System.out.println("Bem-vindo ao Conversor de Moedas!");
+        System.out.println("1. BRL (Real) para USD (Dólar Americano)");
+        System.out.println("2. BRL (Real) para EUR (Euro)");
+        System.out.println("3. BRL (Real) para GBP (Libra Esterlina)");
+        System.out.println("4. BRL (Real) para JPY (Iene Japonês)");
+        System.out.println("5. BRL (Real) para AUD (Dólar Australiano)");
+        System.out.println("6. BRL (Real) para CAD (Dólar Canadense)");
+        System.out.println("7. Sair");
+        System.out.print("Escolha uma opção: ");
+    }
+
     private static String obterMoedaDestino(int escolha) {
         return switch (escolha) {
-            case 1 -> "EUR";
-            case 2 -> "GBP";
-            case 3 -> "JPY";
-            case 4 -> "AUD";
-            case 5 -> "CAD";
-            case 6 -> "BRL";
+            case 1 -> "USD";
+            case 2 -> "EUR";
+            case 3 -> "GBP";
+            case 4 -> "JPY";
+            case 5 -> "AUD";
+            case 6 -> "CAD";
             default -> null;
         };
+    }
+
+    private static void realizarConversao(String moedaDestino) {
+        try {
+            HttpRequest requisicao = HttpRequest.newBuilder()
+                    .uri(URI.create(URL_API))
+                    .build();
+
+            CompletableFuture<HttpResponse<String>> respostaFutura = cliente.sendAsync(requisicao, HttpResponse.BodyHandlers.ofString());
+
+            respostaFutura.thenApply(HttpResponse::body)
+                    .thenAccept(respostaJson -> {
+                        double taxaDeCambio = parseTaxaDeCambio(respostaJson, moedaDestino);
+                        System.out.print("Insira o valor em BRL: ");
+                        double valor = scanner.nextDouble();
+                        double valorConvertido = valor * taxaDeCambio;
+                        System.out.println("Valor convertido: " + valorConvertido + " " + moedaDestino);
+                    }).join();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     private static double parseTaxaDeCambio(String respostaJson, String moedaDestino) {
